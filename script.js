a35c94c230b6952ac62b75d3e123f1aebf169769
indigovsn <133575671+indigovsn@users.noreply.github.com>
2023-05-14 15:05:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+// GSAP Scroll Trigger Animation
+gsap.registerPlugin(ScrollTrigger);
 
+ScrollTrigger.defaults({
+  markers: false
+});
+
+// Animate From To | Text Modelli
+$('.categories_text-wrap').each(function(index) {
+  var triggerElement = $(this);
+  var subtitleElement = triggerElement.find('.categories-subtitle');
+
+  gsap.fromTo(triggerElement, {
+    opacity: 0,
+    paddingLeft: '0em',
+    paddingBottom: '0em',
+    y: '20%',
+    x: '-3%',
+  }, {
+    opacity: 1,
+    paddingLeft: '3em',
+    paddingBottom: '2em',
+    y: '0%',
+    x: '0%',
+    duration: 0.1,
+    ease: 'power2.out',
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom bottom',
+      scrub: 0
+    }
+  });
+
+  gsap.fromTo(subtitleElement, {
+    x: '-10%',
+  }, {
+    x: '0%',
+    duration: 0.1,
+    ease: 'power2.out',
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top bottom',
+      end: 'bottom bottom',
+      scrub: 0
+    }
+  });
+});
